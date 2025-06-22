0c46c7de006afa150e78fd0dbb93a9e07239beb4
ian15963 <ian.santos@sptech.school>
2025-06-22 13:02:40 -0300

    fix: Trocando o send_file pelo upload_image

--- app/service/image_optimizer_service.py
+++ app/service/image_optimizer_service.py
@@ -1,23 +1,21 @@
 import os.path
 from tempfile import NamedTemporaryFile
 
-from flask import send_file, Response
+from .s3_service import upload_image
+from flask import Response
 from werkzeug.datastructures import FileStorage
 from PIL import Image
 
-def optimize_image(image: FileStorage) -> Response:
+def optimize_image(image: FileStorage):
     with NamedTemporaryFile(suffix=os.path.splitext(image.filename)[1], delete=False) as tmp_file:
         image.save(tmp_file)
         tmp_file_path = tmp_file.name
         optimize(tmp_file_path)
 
-    return send_file(tmp_file_path, mimetype="image/*", as_attachment=True,
-                     download_name=image.filename)
-
+    upload_image(tmp_file_path, image.filename)
 
 def optimize(file_name: str):
-    file_extension = file_name.split(".")[1]
-    file_extension = "JPEG" if file_extension.lower().__eq__("jpg") else file_extension.upper()
+    file_extension = _file_extension(file_name)
     try:
         with Image.open(file_name) as img:
             img.save(
@@ -30,3 +28,8 @@ def optimize(file_name: str):
     except Exception as exception:
         print("O seguinte erro ocorreu ao tentar otimizar a imagem:")
         print(f"{exception}")
+
+def _file_extension(file_name):
+    file_extension = file_name.split(".")[1]
+    file_extension = "JPEG" if file_extension.lower().__eq__("jpg") else file_extension.upper()
+    return file_extension
