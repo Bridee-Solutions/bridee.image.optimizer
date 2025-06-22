588b153078f91ee0e9b07e8eeb348e84d754c3cc
ian15963 <ian.santos@sptech.school>
2025-06-21 21:53:29 -0300

    feat: Criando rota

--- app/route/image_optimizer_route.py
+++ app/route/image_optimizer_route.py
@@ -1,3 +1,8 @@
 from flask import Blueprint
+from app.controller import image_optimizer_controller as controller
 
 image_optimizer_blueprint = Blueprint("optimizer", __name__)
+
+@image_optimizer_blueprint.route("/optimize", methods=["POST"])
+def optimize():
+    return controller.optimize_image()
